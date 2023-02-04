dac85f14e43e779ff59f14bd1120e4b539463735
Fábio "DannyIsYog" Sousa <fabio.sousa@tecnico.ulisboa.pt>
2023-02-04 12:46:22 +0000

    Player Controller FIxes

--- Assets/Scripts/Player/AimIndicatorController.cs
+++ Assets/Scripts/Player/AimIndicatorController.cs
@@ -5,25 +5,15 @@ using UnityEngine.InputSystem;
 
 public class AimIndicatorController : MonoBehaviour
 {
-    private PlayerInput playerInput;
-    public GameObject bulletIndicator;
+    public GameObject AimIndicator;
 
-    void Start()
-    {
-        playerInput = GetComponent<PlayerInput>();
-
-        PlayerInputActions playerInputActions = new PlayerInputActions();
-        playerInputActions.Enable();
-        playerInputActions.PlayerActionMap.Aim.performed += Aim_performed;
-    }
-
-    private void Aim_performed(InputAction.CallbackContext obj)
+    public void Aim_performed(InputAction.CallbackContext obj)
     {
         //rotate the bullet indicator according to gamepad obj
         Vector2 aimInput = obj.ReadValue<Vector2>();
         float angle = Mathf.Atan2(aimInput.y, aimInput.x) * Mathf.Rad2Deg;
         angle -= 90;
-        bulletIndicator.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+        AimIndicator.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
     }
 
 
